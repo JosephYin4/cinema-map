45b124787e32e15d3e60a9abe81e639dfaa4bbe5
JosephYin4 <yincc147@hotmail.com>
2026-07-27 13:53:05 +0800

    Update cinemas.js

--- cinemas.js
+++ cinemas.js
@@ -1,48 +1,263 @@
 const cinemas = [
 
-{
-    name:"GV VivoCity",
-    brand:"Golden Village",
-    lat:1.2644,
-    lng:103.8223,
-    mrt:"HarbourFront MRT",
-    address:"1 HarbourFront Walk"
-},
+  // ===== Golden Village =====
+  {
+    name: "GV VivoCity",
+    brand: "Golden Village",
+    lat: 1.2645,
+    lng: 103.8223,
+    mrt: "HarbourFront MRT",
+    address: "1 HarbourFront Walk"
+  },
+  {
+    name: "GV Plaza Singapura",
+    brand: "Golden Village",
+    lat: 1.3006,
+    lng: 103.8455,
+    mrt: "Dhoby Ghaut MRT",
+    address: "68 Orchard Road"
+  },
+  {
+    name: "GV Suntec City",
+    brand: "Golden Village",
+    lat: 1.2947,
+    lng: 103.8572,
+    mrt: "Promenade MRT",
+    address: "3 Temasek Boulevard"
+  },
+  {
+    name: "GV Funan",
+    brand: "Golden Village",
+    lat: 1.2913,
+    lng: 103.8497,
+    mrt: "City Hall MRT",
+    address: "107 North Bridge Road"
+  },
+  {
+    name: "GV Great World",
+    brand: "Golden Village",
+    lat: 1.2939,
+    lng: 103.8318,
+    mrt: "Great World MRT",
+    address: "1 Kim Seng Promenade"
+  },
+  {
+    name: "GV Bugis+",
+    brand: "Golden Village",
+    lat: 1.2998,
+    lng: 103.8554,
+    mrt: "Bugis MRT",
+    address: "201 Victoria Street"
+  },
+  {
+    name: "GV City Square",
+    brand: "Golden Village",
+    lat: 1.3114,
+    lng: 103.8561,
+    mrt: "Farrer Park MRT",
+    address: "180 Kitchener Road"
+  },
+  {
+    name: "GV Paya Lebar",
+    brand: "Golden Village",
+    lat: 1.3176,
+    lng: 103.8925,
+    mrt: "Paya Lebar MRT",
+    address: "10 Paya Lebar Road"
+  },
+  {
+    name: "GV Katong",
+    brand: "Golden Village",
+    lat: 1.3038,
+    lng: 103.9035,
+    mrt: "Marine Parade MRT",
+    address: "112 East Coast Road"
+  },
+  {
+    name: "GV Tampines",
+    brand: "Golden Village",
+    lat: 1.3532,
+    lng: 103.9441,
+    mrt: "Tampines MRT",
+    address: "4 Tampines Central 5"
+  },
+  {
+    name: "GV Century Square",
+    brand: "Golden Village",
+    lat: 1.3525,
+    lng: 103.9447,
+    mrt: "Tampines MRT",
+    address: "2 Tampines Central 5"
+  },
+  {
+    name: "GV Bedok",
+    brand: "Golden Village",
+    lat: 1.3240,
+    lng: 103.9302,
+    mrt: "Bedok MRT",
+    address: "311 New Upper Changi Road"
+  },
+  {
+    name: "GV Bishan",
+    brand: "Golden Village",
+    lat: 1.3509,
+    lng: 103.8487,
+    mrt: "Bishan MRT",
+    address: "9 Bishan Place"
+  },
+  {
+    name: "GV Jurong Point",
+    brand: "Golden Village",
+    lat: 1.3395,
+    lng: 103.7067,
+    mrt: "Boon Lay MRT",
+    address: "1 Jurong West Central 2"
+  },
+  {
+    name: "GV Yishun",
+    brand: "Golden Village",
+    lat: 1.4292,
+    lng: 103.8353,
+    mrt: "Yishun MRT",
+    address: "51 Yishun Central 1"
+  },
+  {
+    name: "GV Cineleisure",
+    brand: "Golden Village",
+    lat: 1.3013,
+    lng: 103.8369,
+    mrt: "Somerset MRT",
+    address: "8 Grange Road"
+  },
 
-{
-    name:"Shaw Lido",
-    brand:"Shaw",
-    lat:1.3048,
-    lng:103.8318,
-    mrt:"Orchard MRT",
-    address:"350 Orchard Road"
-},
+  // ===== Shaw =====
+  {
+    name: "Shaw Lido",
+    brand: "Shaw",
+    lat: 1.3050,
+    lng: 103.8317,
+    mrt: "Orchard MRT",
+    address: "350 Orchard Road"
+  },
+  {
+    name: "Shaw Nex",
+    brand: "Shaw",
+    lat: 1.3508,
+    lng: 103.8720,
+    mrt: "Serangoon MRT",
+    address: "23 Serangoon Central"
+  },
+  {
+    name: "Shaw Waterway Point",
+    brand: "Shaw",
+    lat: 1.4066,
+    lng: 103.9021,
+    mrt: "Punggol MRT",
+    address: "83 Punggol Central"
+  },
+  {
+    name: "Shaw Jewel",
+    brand: "Shaw",
+    lat: 1.3602,
+    lng: 103.9894,
+    mrt: "Changi Airport MRT",
+    address: "78 Airport Boulevard"
+  },
+  {
+    name: "Shaw PLQ",
+    brand: "Shaw",
+    lat: 1.3174,
+    lng: 103.8926,
+    mrt: "Paya Lebar MRT",
+    address: "10 Paya Lebar Road"
+  },
+  {
+    name: "Shaw JEM",
+    brand: "Shaw",
+    lat: 1.3333,
+    lng: 103.7437,
+    mrt: "Jurong East MRT",
+    address: "50 Jurong Gateway Road"
+  },
+  {
+    name: "Shaw Lot One",
+    brand: "Shaw",
+    lat: 1.3854,
+    lng: 103.7443,
+    mrt: "Choa Chu Kang MRT",
+    address: "21 Choa Chu Kang Ave 4"
+  },
+  {
+    name: "Shaw Balestier",
+    brand: "Shaw",
+    lat: 1.3262,
+    lng: 103.8422,
+    mrt: "Novena MRT",
+    address: "100A Balestier Road"
+  },
 
-{
-    name:"Cathay AMK Hub",
-    brand:"Cathay",
-    lat:1.3692,
-    lng:103.8485,
-    mrt:"Ang Mo Kio MRT",
-    address:"53 Ang Mo Kio Ave 3"
-},
+  // ===== Cathay =====
+  {
+    name: "Cathay AMK Hub",
+    brand: "Cathay",
+    lat: 1.3691,
+    lng: 103.8487,
+    mrt: "Ang Mo Kio MRT",
+    address: "53 Ang Mo Kio Ave 3"
+  },
+  {
+    name: "Cathay Causeway Point",
+    brand: "Cathay",
+    lat: 1.4361,
+    lng: 103.7867,
+    mrt: "Woodlands MRT",
+    address: "1 Woodlands Square"
+  },
+  {
+    name: "Cathay Downtown East",
+    brand: "Cathay",
+    lat: 1.3777,
+    lng: 103.9555,
+    mrt: "Pasir Ris MRT",
+    address: "1 Pasir Ris Close"
+  },
 
-{
-    name:"GV Plaza",
-    brand:"Golden Village",
-    lat:1.3007,
-    lng:103.8459,
-    mrt:"Dhoby Ghaut MRT",
-    address:"68 Orchard Road"
-},
+  // ===== WE Cinemas =====
+  {
+    name: "WE Cinemas Clementi",
+    brand: "WE Cinemas",
+    lat: 1.3150,
+    lng: 103.7649,
+    mrt: "Clementi MRT",
+    address: "321 Clementi Ave 3"
+  },
 
-{
-    name:"WE Cinemas Clementi",
-    brand:"WE Cinemas",
-    lat:1.3151,
-    lng:103.7649,
-    mrt:"Clementi MRT",
-    address:"321 Clementi Ave 3"
-}
+  // ===== EagleWings =====
+  {
+    name: "EagleWings Cinematics",
+    brand: "EagleWings",
+    lat: 1.3368,
+    lng: 103.7766,
+    mrt: "King Albert Park MRT",
+    address: "9 King Albert Park"
+  },
+
+  // ===== The Projector =====
+  {
+    name: "The Projector at Golden Mile",
+    brand: "The Projector",
+    lat: 1.3029,
+    lng: 103.8632,
+    mrt: "Nicoll Highway MRT",
+    address: "6001 Beach Road"
+  },
+  {
+    name: "The Projector at Cineleisure",
+    brand: "The Projector",
+    lat: 1.3012,
+    lng: 103.8369,
+    mrt: "Somerset MRT",
+    address: "8 Grange Road"
+  }
 
 ];
